63e3638c0d0f1332009f7af24259cefc7fd37e4e
pgogola <gogola.piotr@interia.pl>
2022-10-03 20:43:46 +0200

    Introduce jira::testing module

--- src/jira/mod.rs
+++ src/jira/mod.rs
@@ -1,13 +1,18 @@
-pub mod client;
-pub mod user_worklogs;
-pub mod user_worklogs_fetcher;
-pub mod user_worklogs_fetcher_trait;
-pub mod worklog;
-
+mod client;
 mod client_trait;
+mod user_worklogs;
+mod user_worklogs_fetcher;
+mod user_worklogs_fetcher_trait;
+mod worklog;
 
 pub use client::Client;
 pub use user_worklogs::UserWorklogs;
 pub use user_worklogs_fetcher::UserWorklogsFetcher;
 pub use user_worklogs_fetcher_trait::UserWorklogsFetcherTrait;
 pub use worklog::Worklog;
+
+#[cfg(test)]
+pub mod testing {
+    pub use super::client_trait::MockClientTrait;
+    pub use super::user_worklogs_fetcher_trait::MockUserWorklogsFetcherTrait;
+}
